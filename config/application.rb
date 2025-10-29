22c00b2a3b7fdb1fe055a1a2702015cfddebcdf2
Diego Isla <riruisla@gmail.com>
2025-10-29 15:28:50 +0100

    delete minitest configuration and files and add rspec generation configuration

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,15 @@ module Socki
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.helper_specs false
+      g.view_specs false
+      g.routing_specs false
+      g.controller_specs false
+    end
+
   end
 end
